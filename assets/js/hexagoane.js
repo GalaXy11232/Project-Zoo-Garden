0764e399cb204361cf39663a707191399de1f6e7
GalaXy11232 <neamtumatei08@gmail.com>
2025-08-11 12:59:46 +0300

    small modifs

--- assets/js/hexagoane.js
+++ assets/js/hexagoane.js
@@ -1,11 +1,11 @@
-const DELAY_STEP = 1.5;
+const DELAY_STEP = Math.PI / 2; // "random" number to avoid time matching
 
 window.addEventListener('load', () => {
     let hex_arr = document.querySelectorAll('.hexagon');
 
-    for (let i = 0; i < hex_arr.length; i++) {
+    for (let i = 0; i < hex_arr.length; i++) {        
         randomizeHexagon(hex_arr[i]);
-        hex_arr[i].addEventListener('animationiteration', randomizeHexagon(hex_arr[i]));
+        hex_arr[i].addEventListener('animationiteration', (_ev) => { randomizeHexagon(hex_arr[i]); });
         hex_arr[i].style.animationDelay = `${i * DELAY_STEP}s`;
     }
 })
@@ -13,8 +13,6 @@ window.addEventListener('load', () => {
 
 function randomizeHexagon(hex) {
     hex.style.left = `${Math.round(Math.random() * window.innerWidth)}px`;
-    hex.style.height = `${50 + Math.round(Math.random() * 25)}px !important`;
-
-
-
+    hex.style.height = `${25 + Math.round(Math.random() * 50)}px`;
+    
 }
