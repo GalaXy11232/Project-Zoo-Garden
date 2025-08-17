0bef3de370cd37fa576fab1fc2c61887ec7ccf37
GalaXy11232 <neamtumatei08@gmail.com>
2025-08-17 22:43:19 +0300

    Update dark_mode.js

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,53 +1,65 @@
 const btn = document.getElementById("theme-toggle");
 const icon = document.getElementById("theme-icon");
 
+const storageThemeEntry = 'system-mode';
+
 var icon_class;
 var theme;
 var r = document.querySelector(":root");
 
-function get_style(var_name) {
-    var rs = getComputedStyle(r);
-    return rs.getPropertyValue(var_name);
-}
+function set_style(property, value) { r.style.setProperty(property, value); }
 
-function switch_style(var_name, light_value, dark_value) {
-  if (localStorage.getItem("theme") === "light") {
-    r.style.setProperty(var_name, light_value);
-  } else {
-    r.style.setProperty(var_name, dark_value);
-  }
-}
+function switch_theme(mode = null) {
+    // just switch to opposite value
+    if (mode === null)
+      mode = localStorage.getItem(storageThemeEntry) == 'light' ? 'dark' : 'light';
+    
 
-document.addEventListener("load", () => {
-    if (localStorage.getItem("theme") !== null) {
-      theme = localStorage.getItem("theme");
-    } else {
-      theme = "light";
-      localStorage.setItem("theme", theme);
-    }
-
-    if (theme === "light") {
+    if (mode == 'light') {
+      theme = mode;
+      localStorage.setItem(storageThemeEntry, theme);
       icon_class = "bi bi-moon";
-    } else {
-      icon_class = "bi bi-sun";
+      icon.className = icon_class;
+      
+      set_style("--primary", "#7b28ae");
+      set_style("--bg-light", "#d1c3e6");
+      set_style("--primary-gradient", "linear-gradient(to right, #7b28ae, #ba12aa)");
+      set_style("--light-text-color", "#fff");
+      set_style("--dark-text-color", "#000");
     }
-    icon.className = icon_class;
+
+    else if (mode == 'dark') {
+      theme = mode;
+      localStorage.setItem(storageThemeEntry, theme);
+      icon_class = "bi bi-sun";
+      icon.className = icon_class;
+
+      set_style("--primary", "#d1c3e6");
+      set_style("--bg-light", "#5b1dc3");
+      set_style("--primary-gradient", "linear-gradient(to right, #ba12aa, #d1c3e6)");
+      set_style("--light-text-color", "#000");
+      set_style("--dark-text-color", "#fff");
+    }
+}
+
+window.addEventListener("load", () => {
+    // create new storage
+    if (localStorage.getItem(storageThemeEntry) === null) {
+      localStorage.setItem(storageThemeEntry, 'light');
+      
+      theme = 'light';
+      switch_theme('light');
+
+      console.log("Created new localstorage for system theme.");
+    }
+
+    // load from existing data
+    else {
+      theme = localStorage.getItem(storageThemeEntry);
+      switch_theme(theme);
+    }
 });
 
 btn.addEventListener("click", function(event) {
-  theme = theme === "light" ? "dark" : "light";
-  localStorage.setItem("theme", theme);
-
-  if (theme === "light") {
-    icon_class = "bi bi-moon";
-  } else {
-    icon_class = "bi bi-sun";
-  }
-  icon.className = icon_class;
-
-  switch_style("--primary", "#7b28ae", "#d1c3e6");
-  switch_style("--bg-light", "#d1c3e6", "#5b1dc3");
-  switch_style("--light-text-color", "#fff", "#000");
-  switch_style("--dark-text-color", "#000", "#fff");
-  switch_style("--primary-gradient", "linear-gradient(to right, #7b28ae, #ba12aa)", "linear-gradient(to right, #ba12aa, #d1c3e6)");
+  switch_theme();
 });
