6893bb5620a0e23ce6029b53165adf88c66fc88e
GalaXy11232 <neamtumatei08@gmail.com>
2025-09-23 22:27:17 +0300

    adaugat voluntari la loc

--- assets/js/echipa.js
+++ assets/js/echipa.js
@@ -1,6 +1,9 @@
 let filterItems = document.querySelectorAll('.item-membru');
 let filterImages = document.querySelectorAll('.box-membru');
 
+let filterVoluntari = document.querySelectorAll('.item-voluntar');
+let filterImagesVoluntari = document.querySelectorAll('.box-voluntar');
+
 window.addEventListener('load', ()=>{
     // pentru membrii echipei
     filterItems.forEach(item => {
@@ -18,6 +21,25 @@ window.addEventListener('load', ()=>{
                     }
                 });
             }
+        });
+    });
+
+    // pentru voluntari
+    filterVoluntari.forEach(item => {
+        item.addEventListener('click', (selectedItem) => {
+            if (selectedItem.target.classList.contains('item-link')) {
+                document.querySelector('.menu-active-v').classList.remove('menu-active-v');
+                selectedItem.target.classList.add('menu-active-v');
+
+                let filterName = selectedItem.target.getAttribute('data-name');
+                filterImagesVoluntari.forEach((image) => {
+                    let imgName = image.getAttribute('data-name');
+                    if (imgName == filterName || (filterName == 'all-v')) { 
+                        image.style.display = 'block'; 
+                    }
+                    else { image.style.display = 'none'; }
+                });
+            };
         });
     });
 });
