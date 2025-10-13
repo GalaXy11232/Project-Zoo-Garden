9e38e87dbaa9b6bde28ee7e6b08f6ed6022c007e
T-Flex09 <filiptiron0@gmail.com>
2025-10-13 16:41:01 +0300

    got rid of redundant scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,11 +24,22 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 //Dynamic content
-const plansData = [
-  { title: "FTC #10 (2025-2026)", description: "Ne pregătim pentru a participa la FIRST Tech Challenge în sezonul 10 din 2025-2026. Vom avea o echipă organizată eficient, alcătuită din membri specializați în diferite domenii și ne propunem să ne calificăm la etapa națională a competiției prin câștigarea unui premiu." },
-  { title: "ONCS 2026", description: "În anul școlar 2025-2026 vom participa la Olimpiada Națională de Creativitate Ştiinţifică cu diferite proiecte care pun în valoare aplicabilitatea excelentă a roboticii în diferite domenii. Acest concurs național ne oferă oportunitatea de a evidenția talentul membrilor noștri și ne propunem să ne calificăm la etapa națională." },
-  { title: "InfoEducație 2026", description: "Pentru următorul an, avem în plan participarea pe mai multe secțiuni în cadrul concursului InfoEducație, unde putem pune în valoare abilitățile digitale și tehnologice diverse ale membrilor clubului nostru." },
-];
+var plansData;
+var path = window.location.pathname;
+
+if (path.includes('/en')) {
+  plansData = [
+    { title: "FTC #10 (2025-2026)", description: "We are preparing to participate in the FIRST Tech Challenge in season 10 of 2025-2026. We will have an efficiently organized team, made up of specialized members in different fields and we aim to qualify for the national stage of the competition by winning a prize." },
+    { title: "NOSC 2026", description: "In the 2025-2026 school year we will participate in the National Olympiad of Scientific Creativity with various projects that highlight the excellent applicability of robotics in different fields. This national competition gives us the opportunity to highlight the talent of our members and we aim to qualify for the national stage." },
+    { title: "InfoEducație 2026", description: "For the next year, we plan to participate in several sections in the InfoEducation contest, where we can highlight the diverse digital and technological skills of our club members." },
+  ];
+} else {
+  plansData = [
+    { title: "FTC #10 (2025-2026)", description: "Ne pregătim pentru a participa la FIRST Tech Challenge în sezonul 10 din 2025-2026. Vom avea o echipă organizată eficient, alcătuită din membri specializați în diferite domenii și ne propunem să ne calificăm la etapa națională a competiției prin câștigarea unui premiu." },
+    { title: "ONCS 2026", description: "În anul școlar 2025-2026 vom participa la Olimpiada Națională de Creativitate Ştiinţifică cu diferite proiecte care pun în valoare aplicabilitatea excelentă a roboticii în diferite domenii. Acest concurs național ne oferă oportunitatea de a evidenția talentul membrilor noștri și ne propunem să ne calificăm la etapa națională." },
+    { title: "InfoEducație 2026", description: "Pentru următorul an, avem în plan participarea pe mai multe secțiuni în cadrul concursului InfoEducație, unde putem pune în valoare abilitățile digitale și tehnologice diverse ale membrilor clubului nostru." },
+  ];
+}
 
 function generateDynamicContent() {
   const container = document.getElementById("dynamic-content");
